27353c9b8690f8f6b2f73abdc0eb9c1e1bba7f53
rbyrne5 <rbyrne5@tcd.ie>
2023-10-16 20:25:18 +0100

    Update run_plink_gwas.py

--- simulations/run_plink_gwas.py
+++ simulations/run_plink_gwas.py
@@ -10,7 +10,7 @@ def epistasis(file_prefix, traits_file):
     run(plink_epistasis_cmd.split(" "))
 
 def pca(file_prefix):
-    plink_ld_prune_cmd = f"plink --bfile {file_prefix} --indep-pairwise 1000 50 0.2 --exclude range high-ld-hg19-regions.txt --allow-no-sex --out {file_prefix}_pruned"
+    plink_ld_prune_cmd = f"plink --bfile {file_prefix} --indep-pairwise 1000 50 0.2 --allow-no-sex --out {file_prefix}_pruned"
     run(plink_ld_prune_cmd.split(" "))
     plink_pca_cmd = f"plink --bfile {file_prefix} --extract {file_prefix}_pruned.prune.in --pca --allow-no-sex --out {file_prefix}_pruned_pca"
     run(plink_pca_cmd.split(" "))
