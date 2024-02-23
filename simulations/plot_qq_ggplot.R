d1c31f246bfcf123c5573ef2047856946f8fee51
rbyrne5 <rbyrne5@tcd.ie>
2024-02-23 01:56:01 +0000

    Update plot_qq_ggplot.R
    Updated with y=x refline (no longer uses qqplotr)

--- simulations/plot_qq_ggplot.R
+++ simulations/plot_qq_ggplot.R
@@ -1,7 +1,6 @@
 library(argparse)
 library(dplyr)
 library(ggplot2)
-library(qqplotr)
 
 # Define the command-line arguments
 parser <- ArgumentParser()
@@ -13,30 +12,84 @@ args <- parser$parse_args()
 data <- read.csv(args$file_path)
 df <- as_tibble(data)
 ntest <- df %>%
-    group_by(adjusted, n_causal, heritability) %>%
-    summarize(n = n())
-dp <- list(rate=log(10))
-di <- "exp"
-de <- FALSE # enabling the detrend option
+  group_by(adjusted, n_causal, heritability) %>%
+  summarize(n = n())
 
-# TODO: change causal snp facet labels
-gg <- df %>% ggplot(mapping = aes(
-  sample = -log10(P),
-  color = adjusted # change colors to SteelBlue and Indianred, Bonferroni line black dashed
-)) +
+#Split into 4 groups to add expected dists and order:
+datasub1 <- data[data$n_causal=="high"&data$adjusted=="unadjusted",]
+datasub2 <- data[data$n_causal=="low"&data$adjusted=="unadjusted",]
+datasub3 <- data[data$n_causal=="high"&data$adjusted=="PCA adjusted",]
+datasub4 <- data[data$n_causal=="low"&data$adjusted=="PCA adjusted",]
+
+##########################################
+###Generate conf int for a line at x=y based on number of SNPs:
+##########################################
+ci <- 0.95
+n_snps <- ntest$n[1]
+
+clower = -log10(qbeta(
+  p = (1 - ci) / 2,
+  shape1 = seq(n_snps),
+  shape2 = rev(seq(n_snps))
+))
+cupper = -log10(qbeta(
+  p = (1 + ci) / 2,
+  shape1 = seq(n_snps),
+  shape2 = rev(seq(n_snps))
+))
+
+#Add conf ints to each parameter set and order by -log(p):
+df1 <- data.frame(observed=-log10(sort(datasub1$P)),
+                 expected= -log10(ppoints(n_snps)),
+                 clower = clower,
+                 cupper = cupper,
+                 n_causal = datasub1$n_causal,
+                 heritability = datasub1$heritability,
+                 adjusted = datasub1$adjusted)
+df2 <- data.frame(observed=-log10(sort(datasub2$P)),
+                   expected= -log10(ppoints(n_snps)),
+                   clower = clower,
+                   cupper = cupper,
+                   n_causal = datasub2$n_causal,
+                   heritability = datasub2$heritability,
+                   adjusted = datasub2$adjusted)
+df3 <- data.frame(observed=-log10(sort(datasub3$P)),
+                  expected= -log10(ppoints(n_snps)),
+                  clower = clower,
+                  cupper = cupper,
+                  n_causal = datasub3$n_causal,
+                  heritability = datasub3$heritability,
+                  adjusted = datasub3$adjusted)
+df4 <- data.frame(observed=-log10(sort(datasub4$P)),
+                  expected= -log10(ppoints(n_snps)),
+                  clower = clower,
+                  cupper = cupper,
+                  n_causal = datasub4$n_causal,
+                  heritability = datasub4$heritability,
+                  adjusted = datasub4$adjusted)
+
+
+#Combine dataframes for plotting:
+df=rbind(df1,df2,df3,df4)
+
+#Plot qq with GWAS style x=y line + conf int:
+gg <- df %>%
+  ggplot(mapping= aes(x = expected, y = observed,color = adjusted)) +
   theme_bw() +
-  stat_qq_band(distribution = di,
-               dparams = dp,
-               detrend = de,
-               alpha = 0.5) +
-  stat_qq_line(distribution = di, dparams = dp, detrend = de) +
-  stat_qq_point(distribution = di, dparams = dp, detrend = de) +
   facet_grid(n_causal ~  adjusted ) +
-  theme(legend.position = "none") +
+  geom_ribbon(aes(ymax = cupper, ymin = clower),
+              fill = "#999999", alpha = 0.5,linewidth =0.25
+  ) +
+  geom_point() +
+  geom_segment(
+    data = . %>% filter(expected == max(expected)),
+    aes(x = 0, xend = expected, y = 0, yend = expected),
+    linewidth = 1, alpha = 1, lineend = "round"
+  ) +
   geom_hline(yintercept=-log10(0.05 / ntest$n[1]), linetype='dashed', col = 'black') +
   labs(x = bquote("Theoretical Quantiles " -log[10](p)),
        y = bquote("Sample Quantiles " -log[10](p))) +
+  theme(legend.position = "none") +
   scale_color_manual(values = c("SteelBlue", "Indianred"))
 
-# TODO: do we need to produce a different file format?
 ggsave(paste0(args$file_path, ".png"), plot = gg, width = 6, height = 4, dpi = 300)
